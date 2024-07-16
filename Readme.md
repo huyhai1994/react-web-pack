edc6c26340f2ed0ad1f5c2703d533c61c319d325
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-16 16:40:56 +0700

    update project

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Cần cài đặt webpack vì chúng ta đang sử dụng webpack để tạo và cài đặt.
+npm i --save-dev webpack webpack-cli webpack-dev-server
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/*TODO: Bước 7: Cài đặt Trình biên dịch, Máy chủ và Trình tải
+        Mở file webpack-config.js và thêm đoạn mã sau.*/
+const path = require("path");
+
+/* thêm html-webpack-plugin vào file cấu hình sẽ được sử dụng ở bên dưới */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js", // Đường dẫn tới file code index.js
+    output: {
+        path: path.resolve(__dirname, "dist"), // Thư mục chứa file được build ra
+        filename: "index.bundle.js", // Tên file được build ra
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Sử dụng babel-loader cho những file .js
+                exclude: /node_modules/, // Loại trừ thư mục node_modules
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    /* cấu hình file index.html từ folder public */
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html")
+        })
+    ]
+};
